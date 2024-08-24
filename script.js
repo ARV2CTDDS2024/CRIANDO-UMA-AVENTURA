c34c1f53344c743d462c238670b88e70bc57ed4b
ARV2CTDDS2024 <00001114467455SP@al.educacao.sp.gov.br>
2024-08-24 15:43:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-<!-- código omitido -->
+const avanca = document.querySelectorAll('.btn-proximo');
 
-    <script src="script.js"></script>
-</body>
-</html>
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
